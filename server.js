9cc04b51fa92d20d2eb8e38601d03497fcfda6f8
Carrie Chow <carriercat@gmail.com>
2012-01-28 23:46:08 -0800

    Changed back to "Settlers of Satan".  Debugged changing names when building settlements or trading.

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ server.listen(8080); //listen on port 8080
 
 var nowjs = require("now"); //create "now"
 var everyone = nowjs.initialize(server); //initialize with server
+var players = {}
 
 everyone.now.takeAction = function(json) {
 	json["owner"] = this.now.name;
@@ -16,33 +17,8 @@ everyone.now.broadcastAttributes = function(attrs) {
 	everyone.now.updatePlayer(this.now.name, attrs);
 }
 
-var players = [{name: "Joey", 
-				hand: {	ore: 0,
-						brick: 0,
-						lumber: 0,
-						wool: 1, 
-						grain: 2}, 
-				cards: {victorypoints: 1,
-						knights: 0,
-						monopolies: 0,
-						yearofplenty: 0,
-						roadbuilding: 0},
-				victorypoints: 4},
-				{name: "Amy",
-				hand: {ore: 2,
-						brick: 2,
-						lumber: 0,
-						wool: 0,
-						grain: 0},
-				cards: {victorypoints: 0,
-						knights: 0,
-						monopolies: 1,
-						yearofplenty: 0,
-						roadbuilding: 0},
-				victorypoints: 5}]
-
 nowjs.on('connect', function(){
-	this.now.updateBlock(players);
+	this.now.updateBlock();
 });
 
 everyone.now.distributeMessage = function(msg){
