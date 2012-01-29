7cd7b233dd4a3a1880572f33886b36ac44fa73bc
hellochar <hellocharlien@hotmail.com>
2012-01-28 21:32:57 -0700

    WOOO

--- server.js
+++ server.js
@@ -6,19 +6,20 @@ server.listen(8080); //listen on port 8080
 
 var nowjs = require("now"); //create "now"
 var everyone = nowjs.initialize(server); //initialize with server
+var allAttributes = {};
 
-
-everyone.now.create = function() {
-	console.log("this.now.name: "+this.now.name);
-	add(new Player(this.now.name));
+everyone.now.takeAction = function(json) {
+	json["owner"] = this.now.name;
+	everyone.now.getAction(json);
 }
 
-everyone.now.syncWorld = function() {
-	this.now.updateClient(world);
+everyone.now.setAttributes = function(attr) {
+	allAttributes[this.now.name] = attr;
 }
 
-everyone.now.update = function(keys, mouse) { //keys is a set of {keyChar}. //mouse is a {x:mouseX, y:mouseY, pressed:mouseButton}
-	inputs[this.now.name] = {keys:keys, mouse:mouse};
+everyone.now.joinGame = function(attr) {
+	allAttributes[this.now.name] = attr;
+	everyone.now.setPlayers(allAttributes);
 }
 
 everyone.now.mousePressed = function(mousePressed, mouseButton, key, keyCode) { //begin charging
