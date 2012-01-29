540049bb6c26a0b2b68c65f72904a064172cdfde
Niraj Amalkanti <neji49@gmail.com>
2012-01-28 16:49:34 -0800

    Added class files for map

---
+++ Edge.java
@@ -0,0 +1,8 @@
+//Edge class for the map
+
+
+Edge {
+    int id;
+    ArrayList<Vertex> vertices; // ArrayList of the faces 
+    ArrayList<Face> faces; //ArrayList of the faces
+    Hashmap graphicsInfo; //Hashtable of the information for GUI
---
+++ Map.java
@@ -0,0 +1,19 @@
+// Initialization class for the map
+
+Map {
+
+    ArrayList<Face> faces;
+    ArrayList<Edge> edges;
+    ArrayList<Vertex> vertices;
+
+    public Map () {
+    }
+
+    private float calculateHexagonLength(float gridHeight) {
+       return gridHeight/(10 * Math.sin(60));
+    }
+
+    private float calculateGridWidth(float gridHeight) {
+        return (3 * gridHeight/10) * ((2/Math.tan(60)) + (1/sin(60)));
+
+    
