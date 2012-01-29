49d2040cf996659c94c3ef154e31729078662fe5
Carrie Chow <carriercat@gmail.com>
2012-01-28 19:43:55 -0800

    Added chat box and dummy player stats display.

--- server.js
+++ server.js
@@ -7,28 +7,35 @@ server.listen(8080); //listen on port 8080
 var nowjs = require("now"); //create "now"
 var everyone = nowjs.initialize(server); //initialize with server
 
+var players = [{name: "Joey", 
+				hand: {	ore: 0,
+						brick: 0,
+						lumber: 0,
+						wool: 1, 
+						grain: 2}, 
+				cards: {victorypoints: 1,
+						knights: 0,
+						monopolies: 0,
+						yearofplenty: 0,
+						roadbuilding: 0},
+				victorypoints: 4},
+				{name: "Amy",
+				hand: {ore: 2,
+						brick: 2,
+						lumber: 0,
+						wool: 0,
+						grain: 0},
+				cards: {victorypoints: 0,
+						knights: 0,
+						monopolies: 1,
+						yearofplenty: 0,
+						roadbuilding: 0},
+				victorypoints: 5}]
 
-everyone.now.create = function() {
-	console.log("this.now.name: "+this.now.name);
-	add(new Player(this.now.name));
-}
+nowjs.on('connect', function(){
+	this.now.updateBlock(players);
+});
 
-everyone.now.syncWorld = function() {
-	this.now.updateClient(world);
-}
-
-everyone.now.update = function(keys, mouse) { //keys is a set of {keyChar}. //mouse is a {x:mouseX, y:mouseY, pressed:mouseButton}
-	inputs[this.now.name] = {keys:keys, mouse:mouse};
-}
-
-everyone.now.mousePressed = function(mousePressed, mouseButton, key, keyCode) { //begin charging
-	var p = players[this.now.name];
-	p.charging = 5;
-}
-
-everyone.now.mouseReleased = function(mousePressed, mouseButton, key, keyCode) { //begin charging
-	var p = players[this.now.name];
-	//shot the web
-	add(new EShot(p, inputs[this.now.name].mouse.x, inputs[this.now.name].mouse.y, p.charging));
-	delete p.charging;
-}
+everyone.now.distributeMessage = function(msg){
+	everyone.now.receiveMessage(this.now.name, msg);	
+};
